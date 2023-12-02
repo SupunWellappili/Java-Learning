6904f82bab7650e938cc2a97d0e8d91b39e7107a
SupunWellappili <supunchanada1814@gmail.com>
2023-12-02 08:44:23 +0530

    char charactor

---
+++ Demo01.java
@@ -0,0 +1,14 @@
+public class Demo01 {
+    public static void main(String[] args) {
+        char ch;
+        ch ='A';
+        System.out.println(ch);//A
+
+
+        char sh =65;
+        System.out.println(sh);//A
+    }
+
+
+
+}
