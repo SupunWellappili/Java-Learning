4c5917a7e5576602cec0c9dd4bfb7aed08759b96
SupunWellappili <supunchandana1814@gmail.com>
2023-12-09 01:28:10 +0530

    Sout Result

---
+++ Demo04.java
@@ -0,0 +1,21 @@
+public class Demo04 {
+    public static void main(String[] args) {
+        int x = 100;
+        double y = 34.23;
+        char ch = 'A';
+        String name = "Supun";
+        Boolean ok = true;
+        double d = 23.45;
+
+        System.out.printf("%d\n", x);
+        System.out.printf("%f\n", y);
+        System.out.printf("%c\n", ch);
+        System.out.printf("%s\n", name);
+        System.out.printf("%b\n", ok);
+        System.out.printf("My Salary is : %f Rs..\n", d);
+        System.out.printf("%60f\n", d);//Width
+        System.out.printf("%20.5f\n", d);//.2 --> 0.00
+        System.out.printf("%-20.5f\n", d);//alignment
+
+    }
+}
