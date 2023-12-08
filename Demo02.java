c519261545880ab1c038a70fedc7d230d6d39698
SupunWellappili <supunchanada1814@gmail.com>
2023-12-08 07:08:31 +0530

    Scanner Updated

--- Demo02.java
+++ Demo02.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class Demo02 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter Your number : ");
+        System.out.print("Enter Your Number : ");
         int num =input.nextInt();
-        System.out.print("Your Number is :" +num);
+        System.out.println("Your Number is :" +num);
+        System.out.println();
+        System.out.println("-----------------");
+        System.out.println();
+        System.out.print("Enter your Age :");
+        double age = input.nextDouble();
+        System.out.println("Your age is :"+age);
     }
 }
