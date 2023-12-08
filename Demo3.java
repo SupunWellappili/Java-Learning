e33ee722442c2af89c3360fe3ff46f37f637e354
SupunWellappili <supunchanada1814@gmail.com>
2023-12-08 07:51:53 +0530

    Non-Primitive Datatypes Values Ranges

---
+++ Demo3.java
@@ -0,0 +1,56 @@
+public class Demo3 {
+    public static void main(String[] args) {
+
+        byte b;
+        b = Byte.MIN_VALUE;
+        System.out.println("Bye Min: "+b);
+
+        b = Byte.MAX_VALUE;
+        System.out.println("Bye Max: "+b);
+
+        System.out.println();
+
+        short s;
+        s = Short.MIN_VALUE;
+        System.out.println("Short Min: " + s);
+
+        s = Short.MAX_VALUE;
+        System.out.println("Short Max: " + s);
+
+        System.out.println();
+
+        int i;
+        i = Integer.MIN_VALUE;
+        System.out.println("Int Min:" + i);
+
+        i = Integer.MAX_VALUE;
+        System.out.println("Int Max:" + i);
+
+        System.out.println();
+
+        long l;
+        l = Long.MIN_VALUE;
+        System.out.println("Long Min: " + l);
+
+        l = Long.MAX_VALUE;
+        System.out.println("Long Max: " + l);
+
+        System.out.println();
+
+        double d;
+        d=Double.MIN_VALUE;
+        System.out.println("Double Min: "+d);
+
+        d = Double.MAX_VALUE;
+        System.out.println("Double Max: "+d);
+
+        System.out.println();
+
+        float f;
+        f =Float.MIN_VALUE;
+        System.out.println("Float Min: "+f);
+
+        f =Float.MAX_VALUE;
+        System.out.println("Float Max: "+f);
+    }
+}
