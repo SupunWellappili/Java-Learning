e7f38bf827bcd5500d4c34c651f63bcfe116cbdc
SupunWellappili <supunchandana1814@gmail.com>
2023-12-09 12:56:05 +0530

    Total Value add

--- Demo03.java
+++ Demo03.java
@@ -1,4 +1,4 @@
-public class Demo3 {
+public class Demo03 {
     public static void main(String[] args) {
 
         byte b;
---
+++ Demo08.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Demo08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Number: ");
+        int num = input.nextInt();
+        int tot = 0;
+        while (num > 0) {
+            System.out.print(num + " + ");
+            tot += num--;
+        }
+        System.out.println("\b\b");
+        System.out.println("Total Value is: " + tot);
+
+    }
+}
