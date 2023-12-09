bc8a8141dc717a951d881a025bc50b06ada12d0d
SupunWellappili <supunchandana1814@gmail.com>
2023-12-09 13:29:01 +0530

    Total Value Multiplication

---
+++ Demo09.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Demo09 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Number: ");
+        int num = input.nextInt();
+        int tot = 1;
+        while (num > 0) {
+            System.out.print(num + " * ");
+            tot *= num--;
+        }
+        System.out.println("\b\b");
+        System.out.println("Total: " + tot);
+    }
+}
