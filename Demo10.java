1e7f0e8d8d90deccda67ae5ca387753b58014051
SupunWellappili <supunchandana1814@gmail.com>
2023-12-13 17:11:52 +0530

    Updated

---
+++ Demo10.java
@@ -0,0 +1,16 @@
+public class Demo10 {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int sh = 100;
+        System.out.println(ch + sh);
+
+
+        System.out.println("--------");
+
+        int x = -100;
+        System.out.println(x);
+        System.out.println(+x);
+        System.out.println(-x);
+
+    }
+}
