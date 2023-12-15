1db5d8070d147d63e5a80fc5a183d159444fc9d3
SupunWellappili <supunchandana1814@gmail.com>
2023-12-16 00:20:03 +0530

    Logical Operators task

---
+++ Logic_Gate/Demo01.java
@@ -0,0 +1,20 @@
+package Logic_Gate;
+
+import java.util.Scanner;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Mark 01 : ");
+        int mark01 = input.nextInt();
+
+        System.out.print("Enter Your Mark 02 : ");
+        int mark02 = input.nextInt();
+
+        if (mark01 >55 || mark02 < 80){
+            System.out.println("Limit Set");
+        }else {
+            System.out.println("Not Set");
+        }
+    }
+}
