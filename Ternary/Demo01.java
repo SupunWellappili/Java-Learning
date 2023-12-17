f424a6f8ac4fcd4863566686dccf7449c1246b44
SupunWellappili <supunchandana1814@gmail.com>
2023-12-17 12:43:54 +0530

    Ternary Operator task 02

--- Ternary/Demo01.java
+++ Ternary/Demo01.java
@@ -9,5 +9,13 @@ public class Demo01 {
 
         int num = input.nextInt();
         System.out.println(num<0 ? -num : num);
+
+        Scanner out = new Scanner(System.in);
+        System.out.print("Enter Your Salary Price : ");
+        int salary = out.nextInt();
+
+      //  salary = salary>=1000 ? salary + 200 : salary+500;
+        salary += salary>=1000 ? 200 : 500;
+        System.out.println(salary);
     }
 }
