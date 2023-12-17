0a75fb4754deb4cd0d908bd7bfa9767b328c24cf
SupunWellappili <supunchandana1814@gmail.com>
2023-12-17 12:59:59 +0530

    Ternatory Operator Task 03

--- Ternary/Demo01.java
+++ Ternary/Demo01.java
@@ -8,14 +8,23 @@ public class Demo01 {
         System.out.print("Enter Your number : ");
 
         int num = input.nextInt();
-        System.out.println(num<0 ? -num : num);
+        System.out.println(num < 0 ? -num : num);
 
         Scanner out = new Scanner(System.in);
         System.out.print("Enter Your Salary Price : ");
         int salary = out.nextInt();
 
-      //  salary = salary>=1000 ? salary + 200 : salary+500;
-        salary += salary>=1000 ? 200 : 500;
+        //  salary = salary>=1000 ? salary + 200 : salary+500;
+        salary += salary >= 1000 ? 200 : 500;
         System.out.println(salary);
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your Credit : ");
+        int credit = in.nextInt();
+
+        System.out.println(credit >= 75 ? "A" : credit >= 65 ? "B" : credit >= 55 ? "C" : credit >= 35 ? "S" : "F");
+
+
     }
 }
