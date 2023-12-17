f88303ab6e394e8d39d1a83ff8ae368bf4ae184e
SupunWellappili <supunchandana1814@gmail.com>
2023-12-17 23:59:47 +0530

    Scope lifecycle

---
+++ Variable/Demo01.java
@@ -0,0 +1,17 @@
+package Variable;
+class Demo_07 {
+    public static void main(String[] args) {
+        int x = 100;
+        System.out.println(x);
+        {
+            int y = 200;
+            System.out.println(x);
+            System.out.println(y);
+            x++;
+            y++;
+
+        }
+        System.out.println(x);
+       // System.out.println(y);//Illegal
+    }
+}
