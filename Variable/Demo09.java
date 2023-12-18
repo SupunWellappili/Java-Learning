fea562340e8837fbd20f1fcab6a16afc5648a81d
SupunWellappili <supunchandana1814@gmail.com>
2023-12-18 16:06:58 +0530

    Variable is Not Intialization in Scope

--- Variable/Demo09.java
+++ Variable/Demo09.java
@@ -3,7 +3,7 @@ package Variable;
 public class Demo09 {
     public static void main(String[] args) {
         final int x;
-        x = 120;
+        x = 120;//Dynamic Value
         System.out.println(x);
         int y;
 
