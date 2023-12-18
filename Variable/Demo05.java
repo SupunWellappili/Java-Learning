b6bb0757a1f47d7ac90ad87a34f26c3acaca56dd
SupunWellappili <supunchandana1814@gmail.com>
2023-12-18 13:11:46 +0530

    Variable Change Condition

---
+++ Variable/Demo05.java
@@ -0,0 +1,20 @@
+package Variable;
+
+import java.util.Scanner;
+
+public class Demo05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your number : ");
+        int num = input.nextInt();
+
+        int x =100;
+        System.out.println(x);
+        int y ;
+        if (num>0){//Variable aka change wenna puluwn nisa
+            y=455;
+        }
+        System.out.println(x);
+        //System.out.println(y);
+    }
+}
