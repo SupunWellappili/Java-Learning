59ec443366863dee72fc89cd9164ce1cd2242acf
SupunWellappili <supunchandana1814@gmail.com>
2023-12-18 13:07:40 +0530

    Variable Initializer Task 02

---
+++ Variable/Demo04.java
@@ -0,0 +1,15 @@
+package Variable;
+
+public class Demo04 {
+    public static void main(String[] args) {
+                int x=100;
+                System.out.println(x);//100
+                int y;
+                if( 10>0)//---------->Legal(Condition is true)
+                {
+                    y=455;
+                }
+                System.out.println(x);//100
+                System.out.println(y);//455
+            }
+        }
