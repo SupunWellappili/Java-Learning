d1d356633f568d91a9d72b96104afda1c264b5a1
SupunWellappili <supunchandana1814@gmail.com>
2023-12-18 13:22:41 +0530

    Final Command Variable Sign

---
+++ Variable/Demo08.java
@@ -0,0 +1,14 @@
+package Variable;
+
+public class Demo08 {
+    public static void main(String[] args) {
+        final int  x =500;
+        System.out.println(x);
+        int y;
+        if (x>0){
+            y=900;
+        }
+        System.out.println(y);
+
+    }
+}
