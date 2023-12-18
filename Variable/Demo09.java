fe2bf545dbfa9a9f49e4b078aefc7907c70d8c5d
SupunWellappili <supunchandana1814@gmail.com>
2023-12-18 15:58:37 +0530

    Variable Intialization use Final Command

---
+++ Variable/Demo09.java
@@ -0,0 +1,19 @@
+package Variable;
+
+public class Demo09 {
+    public static void main(String[] args) {
+        final int x;
+        x = 120;
+        System.out.println(x);
+        int y;
+
+        if (x > 0) {
+            y = 600;
+            System.out.println(y);
+        }
+
+        System.out.println(x);
+       // System.out.println(y);//Illegal
+
+    }
+}
