15afa9607c9ae1c922193509434775a92c6fc7b2
SupunWellappili <supunchandana1814@gmail.com>
2023-12-18 13:20:15 +0530

    Ternary operator Variable Support

---
+++ Variable/Demo07.java
@@ -0,0 +1,12 @@
+package Variable;
+
+import java.util.Scanner;
+
+public class Demo07 {
+    public static void main(String[] args) {
+        int num =-10;
+        int x = num >=0 ? 1 :-1;
+        System.out.println(x); //legal
+
+    }
+}
