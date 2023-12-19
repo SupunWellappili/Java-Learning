37bc36076894bbf26d8cf76a491682d45547e665
SupunWellappili <supunchandana1814@gmail.com>
2023-12-19 15:15:15 +0530

    For Loops Unrechable Task

---
+++ Controller/For/Demo04.java
@@ -0,0 +1,12 @@
+package Controller.For;
+
+public class Demo04 {
+    public static void main(String[] args) {
+        int i =0;
+        for (;;) {
+            System.out.println("A");
+            i++;
+        }
+    //    System.out.println("B");//Unreachable
+    }
+}
