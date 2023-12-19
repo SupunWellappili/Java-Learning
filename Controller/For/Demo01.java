a9382af1e9708a7b176036fef7f518cd228cc260
SupunWellappili <supunchandana1814@gmail.com>
2023-12-19 14:52:13 +0530

    For loops Task 01

---
+++ Controller/For/Demo01.java
@@ -0,0 +1,8 @@
+package Controller.For;
+public class Demo01 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
