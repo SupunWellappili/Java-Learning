942ec75f281f839f070dd47262e280c2c6840de6
SupunWellappili <supunchandana1814@gmail.com>
2023-12-19 15:35:26 +0530

    Deference Statement

---
+++ Controller/For/Demo06.java
@@ -0,0 +1,11 @@
+package Controller.For;
+
+public class Demo06 {
+    public static void main(String[] args) {
+        int i=0;
+        for(;i<10;){
+            System.out.println("VAN");
+            i++;
+        }
+    }
+}
