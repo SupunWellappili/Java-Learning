47eb09697c917976f4549e9df5b23a9a6e580dda
SupunWellappili <supunchandana1814@gmail.com>
2023-12-19 21:47:56 +0530

    Use the Double Value in For loops

---
+++ Controller/For/Demo08.java
@@ -0,0 +1,13 @@
+package Controller.For;
+
+public class Demo08 {
+    public static void main(String[] args) {
+        double d =1.1;
+        System.out.println(d);//1.1
+
+        d++;
+        System.out.println(d);//2.1
+
+
+    }
+}
