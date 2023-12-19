2a1567487067c9ee779c01ad1dffe458421fc5aa
SupunWellappili <supunchandana1814@gmail.com>
2023-12-19 12:58:35 +0530

    Just Literal Typ in Casee

---
+++ Controller/Switch/Demo06.java
@@ -0,0 +1,25 @@
+package Controller.Switch;
+
+import java.util.Scanner;
+
+public class Demo06 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Num : ");
+        int num = input.nextInt();
+
+        int a = 100;
+        final int b = 500;
+
+        final int c;
+        c = 900;
+
+
+        switch (num) {
+            case b:
+                // case a:
+                // case c:
+
+        }
+    }
+}
