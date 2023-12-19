ea052ff13557f5e6669e58dd6784c3c21097e183
SupunWellappili <supunchandana1814@gmail.com>
2023-12-19 15:32:03 +0530

    For Loops Rechable Task

---
+++ Controller/For/Demo05.java
@@ -0,0 +1,11 @@
+package Controller.For;
+
+public class Demo05 {
+    public static void main(String[] args) {
+        System.out.println("start");
+        for (byte j = 0; ; j++) {
+            System.out.println(j);
+            j++;
+        }
+    }
+}
