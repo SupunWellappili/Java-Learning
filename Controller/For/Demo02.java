e1d927d7a324786b46f86aeab60c9af9fd51bcba
SupunWellappili <supunchandana1814@gmail.com>
2023-12-19 15:01:10 +0530

    For loop behaviours

---
+++ Controller/For/Demo02.java
@@ -0,0 +1,10 @@
+package Controller.For;
+
+public class Demo02 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++)
+            System.out.println("A");
+            System.out.println("B");
+
+    }
+}
