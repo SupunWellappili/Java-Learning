984ac092067cfa85957590346463013cd0857c73
SupunWellappili <supunchandana1814@gmail.com>
2023-12-19 14:27:19 +0530

    Updated

--- Controller/Switch/Demo07.java
+++ Controller/Switch/Demo07.java
@@ -40,5 +40,23 @@ public class Demo07 {
             default:
                 System.out.println("Your Number is Wrong!");
         }
+        System.out.println("------------------");
+
+        switch (num){
+            case 11:
+                System.out.println("Number 11");break;
+            case 12:
+                System.out.println("Number 12");break;
+            case 13:
+                System.out.println("Number 13");break;
+            case 14:
+                System.out.println("Number 14"); break;
+            case 15:
+                System.out.println("Number 15");break;
+            case 16:
+                System.out.println("Number 16");break;
+            case 17:
+                    System.out.println("number 17");break;
+        }
     }
 }
