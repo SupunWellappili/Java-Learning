3d3a94aae4d9d83b781ccc1b22858e119dbeba36
SupunWellappili <supunchandana1814@gmail.com>
2023-12-19 21:43:32 +0530

    For Loops Dehaviours

---
+++ Controller/For/Demo07.java
@@ -0,0 +1,25 @@
+package Controller.For;
+public class Demo07 {
+    public static void main(String[] args) {
+        for (byte y = 0; y<100 ; y++) {
+            System.out.println((y+1)+" Count");
+        }
+
+        System.out.println("----------------");
+
+        int i=0;
+        for (;i<10;){
+            System.out.println("XYZ");
+            i++;
+        }
+
+        System.out.println("----------------");
+
+        int j=0;
+        for (;;){
+            System.out.println("ABC");
+            j++;
+        }
+    }
+}
+
