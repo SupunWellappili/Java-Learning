a0c5b825336d6518b530a770c11b2d9ce10c71f2
SupunWellappili <supunchandana1814@gmail.com>
2023-12-19 15:03:01 +0530

    Updated

--- Controller/For/Demo02.java
+++ Controller/For/Demo02.java
@@ -1,10 +1,19 @@
 package Controller.For;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class Demo02 {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++)
             System.out.println("A");
             System.out.println("B");
 
+
+        System.out.println("-------------");
+
+        for (int j = 0; j < 10; j++) {
+            System.out.println("P");
+            System.out.println("Q");
+        }
     }
 }
