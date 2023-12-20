019a55837f351f1b38303c6cc15d83c12551a3bf
SupunWellappili <supunchandana1814@gmail.com>
2023-12-20 22:54:55 +0530

    Char Values And Number

---
+++ Controller/For/Demo10.java
@@ -0,0 +1,11 @@
+package Controller.For;
+
+import sun.font.FontRunIterator;
+
+public class Demo10 {
+    public static void main(String[] args) {
+        for (char ch = 'A'; ch < 'Z'; ch++) {
+            System.out.println((int)ch+") "+ch);
+        }
+    }
+}
