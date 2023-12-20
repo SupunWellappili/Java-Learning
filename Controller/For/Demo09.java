11326995b01cd7e6a40b66f73aa67b648dec9eb1
SupunWellappili <supunchandana1814@gmail.com>
2023-12-20 22:26:11 +0530

    For Loop Unreable

---
+++ Controller/For/Demo09.java
@@ -0,0 +1,13 @@
+package Controller.For;
+
+public class Demo09 {
+    public static void main(String[] args) {
+
+        System.out.println("Start");
+        for (byte i = 0; ; ) { //Legal
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
