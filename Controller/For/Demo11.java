92b568bcb5272f1c24125bd65b4d79fa6cfb46b5
SupunWellappili <supunchandana1814@gmail.com>
2023-12-20 22:58:02 +0530

    Use i and j Variable in For loops

---
+++ Controller/For/Demo11.java
@@ -0,0 +1,9 @@
+package Controller.For;
+
+public class Demo11 {
+    public static void main(String[] args) {
+        for (int i = 0,j=10; i <10 ; i++,j--) {
+            System.out.println(i+" "+j);
+        }
+    }
+}
