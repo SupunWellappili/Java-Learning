a02bcaaaf760f2619f78898b99448807171ebd50
SupunWellappili <supunchandana1814@gmail.com>
2023-12-21 19:15:58 +0530

    For Loops \b\b

---
+++ Controller/For/Demo12.java
@@ -0,0 +1,11 @@
+package Controller.For;
+
+public class Demo12 {
+    public static void main(String[] args) {
+        System.out.print("[");
+        for (int i = 0; i <=9 ; i++) {
+            System.out.print(i+", ");
+        }
+        System.out.println("\b\b]");
+    }
+}
