6ae90ed2194e3817b2a96fc0f4aaf14a41ab5b0c
SupunWellappili <supunchandana1814@gmail.com>
2023-12-23 23:43:07 +0530

    Break 99 Value

---
+++ Random/Demo05.java
@@ -0,0 +1,16 @@
+
+package Random;
+import java.util.Random;
+class Demo05 {
+    public static void main(String args[]) {
+        Random input = new Random();
+        for (int i = 0; i < 200; i++) {
+            int num = input.nextInt(100);
+            System.out.println(num);
+            if (num==99){
+                break;
+            }
+        }
+    }
+}
+
