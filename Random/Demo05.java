396715afd270934548618eaf6466e94982087a09
SupunWellappili <supunchandana1814@gmail.com>
2023-12-25 23:38:29 +0530

    1-100 Value Range and Break 99 Value

--- Random/Demo05.java
+++ Random/Demo05.java
@@ -1,16 +1,20 @@
-
 package Random;
+
 import java.util.Random;
+
 class Demo05 {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Random input = new Random();
         for (int i = 0; i < 200; i++) {
             int num = input.nextInt(100);
-            System.out.println(num);
-            if (num==99){
+            System.out.println("Enter Value -: "+num);
+            if (num ==99){
                 break;
             }
+                System.out.println(num);
         }
     }
 }
+
+
 
