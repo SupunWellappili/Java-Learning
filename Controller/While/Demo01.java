c8a9768ec0c94d73cc2081b506ffc3049770e0f7
SupunWellappili <supunchandana1814@gmail.com>
2023-12-26 00:18:46 +0530

    print HELLO WORD in while loop

---
+++ Controller/While/Demo01.java
@@ -0,0 +1,11 @@
+package Controller.While;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        int i=1;
+        while (i<=10){
+            System.out.println(i+") Hello World!!!");
+            i++;
+        }
+    }
+}
