9ac3a339790828c1fcf0644bbbd8cbe894787425
SupunWellappili <supunchandana1814@gmail.com>
2023-12-26 12:44:19 +0530

    random numbers 0-100, stop 100 value print in for loops

---
+++ Controller/While/Demo04.java
@@ -0,0 +1,13 @@
+package Controller.While;
+
+import java.util.Random;
+
+public class Demo04 {
+    public static void main(String[] args) {
+        Random input = new Random();
+        for (int i = 0; i != 100;) {
+            i = input.nextInt(101);
+            System.out.println(i);
+        }
+    }
+}
