6c9368c7ac759cc85a12a2d5e61be2e92a06b073
SupunWellappili <supunchandana1814@gmail.com>
2023-12-26 17:36:10 +0530

    Method Behavious

---
+++ Method/Demo01.java
@@ -0,0 +1,15 @@
+package Method;
+
+public class Demo01 {
+    public static void printAddress(){
+        System.out.println("Supun Wellappili");
+        System.out.println("Galle");
+    }
+
+    public static void main(String[] args) {
+        printAddress();
+        System.out.println();
+        printAddress();
+        printAddress();
+    }
+}
