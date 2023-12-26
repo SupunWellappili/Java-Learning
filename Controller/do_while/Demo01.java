6319a117a2f2645b4d4943d148382ac0064369b6
SupunWellappili <supunchandana1814@gmail.com>
2023-12-26 16:25:00 +0530

    Do-While loop example

---
+++ Controller/do_while/Demo01.java
@@ -0,0 +1,16 @@
+package Controller.do_while;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        int i = 10;
+        while(i<10){
+            System.out.println("While loops Number : "+i);
+        }
+
+        int j=10;
+        do{
+            System.out.println("do/while loops Number : "+j);
+        }while(j<10);
+
+    }
+}
