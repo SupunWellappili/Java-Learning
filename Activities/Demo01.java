6174015607f2c81999ddd532134dc9d2811565fa
SupunWellappili <supunchandana1814@gmail.com>
2023-12-27 22:02:15 +0530

    updated

--- Activities/Demo01.java
+++ Activities/Demo01.java
@@ -1,2 +1,23 @@
-package Activities;public class Demo01 {
+package Activities;
+
+import java.util.Scanner;
+
+public class Demo01 {
+    public static int sumOfDigits(int num) {
+        int sum = 0;
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your number -: ");
+        int num = input.nextInt();
+
+        int sum;
+        sum=sumOfDigits(num);
+        System.out.println("Sum Of Digits For -: "+num+" --> "+sum);
+    }
 }
