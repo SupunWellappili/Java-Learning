763f0a75bfe578aaf071c97cc50abf30a9087264
SupunWellappili <supunchandana1814@gmail.com>
2023-12-28 14:27:10 +0530

    About Method Declaration

---
+++ Method_Declaration/Demo01.java
@@ -0,0 +1,23 @@
+package Method_Declaration;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        myMethod();
+        Demo01.myMethod();
+        myClass.myMethod();
+    }
+
+    public static void myMethod() {
+        System.out.println("Hello C++");
+
+    }
+}
+
+
+class myClass {
+    public static void myMethod() {
+        System.out.println("Hello C#");
+    }
+}
+
+
