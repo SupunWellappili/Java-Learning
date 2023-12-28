6265b7e94ba4167172fb4f19ee2bb4fa9da566f2
SupunWellappili <supunchandana1814@gmail.com>
2023-12-28 11:16:52 +0530

    Method Calling Stack

---
+++ Activities/Demo03.java
@@ -0,0 +1,13 @@
+package Activities;
+
+public class Demo03 {
+    public static void myMethod(){
+        System.out.println("myMethod Star");
+        System.out.println("myMethod End");
+    }
+    public static void main(String[] args) {
+        System.out.println("Start Main");
+        myMethod();
+        System.out.println("End Main");
+    }
+}
