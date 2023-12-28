66b83de3faae7a9f2d8e7ceeaa2b728c2afc8169
SupunWellappili <supunchandana1814@gmail.com>
2023-12-28 17:39:05 +0530

    README

--- Method_Declaration/Demo05.java
+++ Method_Declaration/Demo05.java
@@ -1,23 +1,20 @@
 package Method_Declaration;
 
 public class Demo05 {
+
     public static void main(String[] args) {
         Demo05 ob = new Demo05();
         int x = 100;
         int y = 2300;
         System.out.println(x + " : " + y);
         int xx = ob.getting(x, y);
-        System.out.println(x + " : " + y+" --> "+"New X :"+xx);
-
-
+        System.out.println(x + " : " + y + " --> " + "New X :" + xx);
     }
 
     public int getting(int x, int y) {
         System.out.println("Class to Class");
-        System.out.println(x+" : "+y);
+        System.out.println(x + " : " + y);
         x++;
         return x;
-
-
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Java-Learning
 Java
+
+
+import java.util.Scanner;
+class Demo{
+public  static void main (String args[]){
+
+	}
+	public static  void getting(int x, y){
+//parametar athule coma separatar use krnna baa nikan
+int x,y,z;  //illegal
+}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class main {
+public static void main(String args[]) {
+int x=78,y=9;
+main ob = new main();
+ob.getting(x,y);
+}
+
+    public void getting(int p, int q) {
+        int a;
+        int b;
+        int p; //illegal
+        System.out.println(p+"  "+q);
+    }
+}
