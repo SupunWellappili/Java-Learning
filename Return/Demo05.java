841014f860d3fddecc5f7d4c3e7e2e63b6de6db7
SupunWellappili <supunchandana1814@gmail.com>
2024-01-01 07:45:29 +0530

    Find Grade in point

---
+++ Return/Demo05.java
@@ -0,0 +1,28 @@
+package Return;
+
+import java.util.Scanner;
+
+public class Demo05 {
+    public static char findGrade(double avg) {
+        if (avg > 74) {
+            return 'A';
+        } else if (avg > 64) {
+            return 'B';
+        } else if (avg > 54) {
+            return 'C';
+        } else if (avg > 34) {
+            return 'S';
+        } else {
+            return 'F';
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Inter your Mark -: ");
+        double point = input.nextInt();
+
+        char grade = findGrade(point);
+        System.out.println("Final Result : " + grade);
+    }
+}
