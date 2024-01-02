e1105a3b7232b0c0472f1288677fb33257ff748d
SupunWellappili <supunchandana1814@gmail.com>
2024-01-02 11:41:20 +0530

    find grade in return(Ternary Opertor)

---
+++ Activities/Demo09.java
@@ -0,0 +1,24 @@
+package Activities;
+
+
+import java.util.Scanner;
+
+public class Demo09 {
+
+    public static char findGrade(double avg) {
+        return (avg >= 75 ? 'A' : avg > 65 ? 'B' : avg >= 55 ? 'C' : avg >= 35 ? 'S' : 'F');
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Grade -: ");
+        double avg = input.nextDouble();
+
+        System.out.println("Result : "+findGrade(avg));
+
+
+    }
+
+}
