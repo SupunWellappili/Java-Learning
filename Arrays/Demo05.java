d3f856e00a8a71626b6019dc6afdfa11520eb0d3
SupunWellappili <supunchandana1814@gmail.com>
2024-01-02 23:24:48 +0530

    Arrays task 03

---
+++ Arrays/Demo05.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Demo05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Number -: ");
+        final int N = input.nextInt();
+
+        //Random Value Limit
+        int[] xr = new int[N];
+        Random output = new Random();
+        for (int i = 0; i < N; i++) {
+            xr[i] = output.nextInt(100);
+            System.out.println((i + 1) + ") " + xr[i]);
+        }
+
+        //Total Value
+        int tot = 0;
+        for (int i = 0; i < N; i++) {
+            tot += xr[i];
+        }
+        System.out.println("Total Value is - : " + tot);
+
+
+        //Max Value
+        int max = xr[0];
+        for (int i = 0; i < N; i++) {
+            if (xr[i] > max) {
+                max = xr[i];
+            }
+        }
+        System.out.println("Maximum Value is -: " + max);
+    }
+}
