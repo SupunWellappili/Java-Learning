75f5ed0acafc889c9da8c20ca5fe4d10ed82c334
SupunWellappili <supunchandana1814@gmail.com>
2024-01-02 14:41:29 +0530

    Arrays task 01

---
+++ Arrays/Demo04.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Demo04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] xr = new int[10];
+        int tot = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter number " + (i + 1) + " -: ");
+            xr[i] = input.nextInt();
+            tot += xr[i];
+        }
+        System.out.println("Total is : " + tot);
+    }
+}
