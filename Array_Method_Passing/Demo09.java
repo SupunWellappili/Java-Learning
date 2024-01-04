69dff1c119edb3ac85ef607fd5d706687e5ec895
SupunWellappili <supunchandana1814@gmail.com>
2024-01-04 13:26:48 +0530

    For Loops Task

---
+++ Array_Method_Passing/Demo09.java
@@ -0,0 +1,17 @@
+package Array_Method_Passing;
+
+import java.util.Arrays;
+
+public class Demo09 {
+    public static void main(String[] args) {
+        int[] salary = {30000, 45000, 23000, 56000, 60000, 13000};
+        System.out.println(Arrays.toString(salary));
+        System.out.println("New Salary -: ");
+        //increment Salary by 10%
+        //Insert Code Here
+        for (int i = 0; i < salary.length; i++) {
+            salary[i] += salary[i] * 0.1;
+        }
+        System.out.println(Arrays.toString(salary));
+    }
+}
