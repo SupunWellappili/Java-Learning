04a98076b92833b7e7b507ccea3eba12b17be2db
SupunWellappili <supunchandana1814@gmail.com>
2024-01-10 00:10:37 +0530

    2D arrys reference Value Checking

---
+++ Multi_Dimensional_Arrays/Demo02.java
@@ -0,0 +1,31 @@
+package Multi_Dimensional_Arrays;
+
+public class Demo02 {
+    public static void main(String[] args) {
+        int[] ar;
+        ar = new int[4];
+        System.out.println(ar);
+        System.out.println(0);
+
+        System.out.println("/////////////////");
+
+        int[][] br;
+        br =new int[4][3];
+        System.out.println(br);
+        System.out.println(br[0]);
+        System.out.println(br[0][0]);
+
+        System.out.println("/////////////////");
+
+        char[] cr;
+        cr=new char[5];
+        System.out.println(cr);
+        System.out.println(cr[0]);
+     //   System.out.println(cr[0][0]);
+
+        int grade = cr[0];
+        System.out.println(grade);
+
+
+    }
+}
