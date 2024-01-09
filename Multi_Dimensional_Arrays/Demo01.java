39eed286caafda63e1443797729346fbffc95ce8
SupunWellappili <supunchandana1814@gmail.com>
2024-01-09 23:23:31 +0530

    Updated

--- Multi_Dimensional_Arrays/Demo01.java
+++ Multi_Dimensional_Arrays/Demo01.java
@@ -7,10 +7,17 @@ public class Demo01 {
         int[][] x = new int[4][3];
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Insert Number : ");
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Input Marks Of Student-: " + (i + 1) + " ");
+            for (int j = 0; j < 3; j++) {
+                System.out.println("\tSubject-: " + (j + 1) + " ");
+                x[i][j] = input.nextInt();
+            }
+        }
 
 
-        x[0][0] = input.nextInt();
+        /*x[0][0] = input.nextInt();
         x[0][1] = input.nextInt();
         x[0][2] = input.nextInt();
 
@@ -24,6 +31,6 @@ public class Demo01 {
 
         x[3][0] = input.nextInt();
         x[3][1] = input.nextInt();
-        x[3][2] = input.nextInt();
+        x[3][2] = input.nextInt();*/
+        }
     }
-}
