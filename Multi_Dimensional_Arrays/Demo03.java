1d07896b058c7af107b5974b2c8b44bb4a54dff2
SupunWellappili <supunchandana1814@gmail.com>
2024-01-10 00:54:12 +0530

    Arrays Asign Default Values

--- Multi_Dimensional_Arrays/Demo03.java
+++ Multi_Dimensional_Arrays/Demo03.java
@@ -3,11 +3,8 @@ package Multi_Dimensional_Arrays;
 
 public class Demo03 {
         public static void main(String args []){
-            char[] grade = new char[5];
-            System.out.println("char is : null character "+grade[0]);
-
-            int code = grade[0];
-            System.out.println("ASIIC Code : "+code);
+            int[] x =new int[3];
+            System.out.println(x[0]+", "+x[1]+", "+x[2]);//0,0,0
         }
     }
 
