e8eb02402f6b93989a7cd54e608813f272b45470
SupunWellappili <supunchandana1814@gmail.com>
2024-01-09 13:55:03 +0530

    Updated

--- Array_Searching/Demo03.java
+++ Array_Searching/Demo03.java
@@ -1,4 +1,5 @@
 package Array_Searching;
+/*
 import java.util.Arrays;
 
 public class Demo03 {
@@ -19,3 +20,29 @@ public class Demo03 {
 
     }
 }
+*/
+
+
+import java.util.Arrays;
+
+class Example {
+    public static boolean search(int[] a, int b) {
+        for (int i = 0; i < a.length; i++) {
+            if (b == a[i]) {
+                return true;
+            }else {
+              //  return false;//error bug danna ba else 1kak
+            }
+        }return  false;//meya danne element zero u witadi return statment 1ka thiyenna ona
+    }
+
+
+    public static void main(String[] args) {
+        int[] xr = {43, 23, 54, 56, 76, 89, 56, 87, 26, 59, 21, 72};
+        System.out.println(Arrays.toString(xr));
+
+        System.out.println("56 is exist :" + search(xr, 56));//true
+        System.out.println("99 is exist :" + search(xr, 99));//false
+    }
+}
+
