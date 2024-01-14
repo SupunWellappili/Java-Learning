225cff0c9f9bfb9a336a015780fe37df5aca7d3e
SupunWellappili <supunchandana1814@gmail.com>
2024-01-14 13:04:29 +0530

    Stack Over Flow Error

---
+++ Method/Demo07.java
@@ -0,0 +1,14 @@
+package Method;
+
+public class Demo07 {
+
+    public static void printValue(int i){
+        System.out.println("i : "+i);
+        printValue(i++);
+
+    }
+    public static void main(String[] args) {
+        printValue(0);
+    }
+}
+//StackOverFlowError
