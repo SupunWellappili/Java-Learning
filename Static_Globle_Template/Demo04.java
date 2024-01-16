22a37d87de3eed23b94bc6b37d85047ca35f1c75
SupunWellappili <supunchandana1814@gmail.com>
2024-01-16 23:59:28 +0530

    Using static reference variable

---
+++ Static_Globle_Template/Demo04.java
@@ -0,0 +1,20 @@
+package Static_Globle_Template;
+
+import java.util.Arrays;
+
+public class Demo04 {
+    static int[] xr = {10, 20, 30, 40, 50};
+
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(xr));
+        Increment();
+        System.out.println(Arrays.toString(xr));
+    }
+
+    public static void Increment() {
+        for (int i = 0; i < xr.length; i++) {
+            xr[i]++;
+        }
+    }
+}
